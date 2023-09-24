aff1ee45ebc498d366d158a8b2297ec3a010e4a7
Shay Hahiashvili <shayhh@outlook.com>
2023-09-24 09:52:17 +0300

    added icon

--- FileShredder/main.cpp
+++ FileShredder/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("images/fileShredder.ico")); //add icon
     FileShredder w;
     w.show();
     return a.exec();
