dc3581b084c628d32160550447222020da1a1568
Shay Hahiashvili <shayhh@outlook.com>
2025-09-10 19:15:10 +0300

    Upgraded to Qt6 due to Qt5 end of life. Works better with new graphics and faster GUI animations.

--- CryptoShredder/File.cpp
+++ CryptoShredder/File.cpp
@@ -8,113 +8,44 @@ bool File::isFailed = false; //initialization of static isFailed flag
 /**
  * @brief Constructor of class.
  */
-File::File(const string& filePath, Observer& observer) : Observable() {
-    filesystem::path p(File::ToWString(filePath)); //create path object to get info from filePath
-    if (!exists(p)) { //check if the path exists
-        throw runtime_error("Error, the path: " + filePath + " does not exist."); //if not we throw runtime error
-    }
-    this->name = p.stem().wstring(); //set the name of file
-    this->extention = p.extension().wstring(); //set the extention 
-    this->fullName = p.stem().wstring() + p.extension().wstring(); //set the file full name
-    this->fullPath = p.wstring(); //set full path of file
-    this->length = filesystem::file_size(p); //set the length with file_size method
-    this->addObserver(&observer); //add the observer to observer list
+File::File(const filesystem::path& filePath) : Observable() {
+    this->name = filePath.stem().u16string(); //set the name of file
+    this->extention = filePath.extension().u16string(); //set the extention 
+    this->fullName = filePath.stem().u16string() + filePath.extension().u16string(); //set the file full name
+    this->fullPath = filePath.u16string(); //set full path of file
+    this->length = filesystem::file_size(filePath); //set the length with file_size method
 }
 
 
 /**
- * @brief Function to convert from wstring to regular string.
- * @param wstring wstr
+ * @brief Function that removes a file from computer storage.
+ * @param File* file
+ * @throws runtime_error if file does not exist or error occurs.
  */
-string File::ToString(const wstring& wstr) {
-    string result = "";
-    for (wchar_t wc : wstr) {
-        if (wc <= 0x7F) {
-            result.push_back((char)wc); //single-byte characters
-        }
-        else if (wc <= 0x7FF) {
-            result.push_back((char)(0xC0 | (wc >> 6))); //first byte for multi-byte characters
-            result.push_back((char)(0x80 | (wc & 0x3F))); //second byte for multi-byte characters
-        }
-        else if (wc <= 0xFFFF) {
-            result.push_back((char)(0xE0 | (wc >> 12))); //first byte for 3-byte characters
-            result.push_back((char)(0x80 | ((wc >> 6) & 0x3F))); //second byte
-            result.push_back((char)(0x80 | (wc & 0x3F))); //third byte
-        }
-        else if (wc <= 0x10FFFF) {
-            result.push_back((char)(0xF0 | (wc >> 18))); //first byte for 4-byte characters
-            result.push_back((char)(0x80 | ((wc >> 12) & 0x3F))); //second byte
-            result.push_back((char)(0x80 | ((wc >> 6) & 0x3F))); //third byte
-            result.push_back((char)(0x80 | (wc & 0x3F))); //fourth byte
-        }
+void File::removeFile(File* file) {
+    try {
+		if (!filesystem::remove(file->fullPath)) //try to remove the file, if true then file does not exist
+			throw runtime_error("Thread: Failed deleting file, file does not exist."); //throw runtime error
     }
-    return result;
-}
-
-
-/**
- * @brief Function to convert from string to wstring.
- * @param string str
- */
-wstring File::ToWString(const string& str) {
-    wstring result = L"";
-    size_t i = 0;
-    while (i < str.size()) {
-        unsigned char c1 = str[i];
-        if (c1 <= 0x7F) {
-            result.push_back((wchar_t)c1); //single-byte characters
-            ++i;
-        }
-        else if ((c1 >> 5) == 0x6) {
-            unsigned char c2 = str[i + 1];
-            wchar_t wc = ((c1 & 0x1F) << 6) | (c2 & 0x3F); //2-byte character
-            result.push_back(wc);
-            i += 2;
-        }
-        else if ((c1 >> 4) == 0xE) {
-            unsigned char c2 = str[i + 1];
-            unsigned char c3 = str[i + 2];
-            wchar_t wc = ((c1 & 0xF) << 12) | ((c2 & 0x3F) << 6) | (c3 & 0x3F); //3-byte character
-            result.push_back(wc);
-            i += 3;
-        }
-        else if ((c1 >> 3) == 0x1E) {
-            unsigned char c2 = str[i + 1];
-            unsigned char c3 = str[i + 2];
-            unsigned char c4 = str[i + 3];
-            wchar_t wc = ((c1 & 0x7) << 18) | ((c2 & 0x3F) << 12) | ((c3 & 0x3F) << 6) | (c4 & 0x3F); //4-byte character
-            result.push_back(wc);
-            i += 4;
-        }
-    }
-    return result;
-}
-
-
-/**
- * @brief Function to remove a file from computer storage.
- * @param File file
- */
-void File::removeFile(const File& file) {
-    if (_wremove(file.fullPath.c_str()) != 0) { //if true we failed removing file
-        throw runtime_error("Error trying to delete file: " + File::ToString(file.fullPath)); //throw exception with error
+	catch (const filesystem::filesystem_error& e) { //catch filesystem error that may be thrown
+		throw runtime_error(string("Thread: Error deleting file: ") + e.what()); //throw runtime error
     }
 }
 
 
 /**
- * @brief Function that handles wiping the file contents securely using crypto methods.
- * @param File file
+ * @brief Function that handles wiping the file contents securely using cryptographically secure random bytes.
+ * @param File* file
  * @param int passes
  * @param bool toRemove
  */
-void File::WipeFile(const File& file, int passes, bool toRemove) {
-    fstream outputFile(file.fullPath, ios::in | ios::out | ios::binary); //open the file in binary mode for reading and writing
+void File::WipeFile(File* file, int passes, bool toRemove) {
+    fstream outputFile(filesystem::path(file->fullPath), ios::in | ios::out | ios::binary); //open the file in binary mode for reading and writing
 
     if (!outputFile.is_open() || outputFile.fail()) { //we check if we failed opening the file
         outputFile.close(); //close the file due to error
         File::setIsFailed(true); //set isFailed to true to indicate of failure
-        file.notify(false); //notify all observers that we finished the task
+        file->notify(false); //notify all observers that we finished the task
         return; //finish the function if error occured
     }
 
@@ -122,10 +53,10 @@ void File::WipeFile(const File& file, int passes, bool toRemove) {
         random_device randomDevice; //for random bytes generator
         mt19937_64 generator(randomDevice()); //secure random byte generator
 
-        size_t fileSize = file.length; //set fileSize to be file size in bytes
+        size_t fileSize = file->length; //set fileSize to be file size in bytes
         size_t currentSize = 0; //set currentSize to be zero to indicate the beginning of file
         size_t chunkSize = 0; //set chunkSize to be zero and later calculate minimal chunk to read
-        const size_t maxBufferSize = 1024 * 1024; //set maxBufferSize to be 1MB for efficiency
+        const size_t maxBufferSize = 1024LL * 1024LL; //set maxBufferSize to be 1MB for efficiency
         size_t bufferSize = min(fileSize, maxBufferSize); //set minimal bufferSize for memory efficiency
         vector<char> buffer(bufferSize); //create buffer vector based on bufferSize
 
@@ -140,7 +71,7 @@ void File::WipeFile(const File& file, int passes, bool toRemove) {
 
                 if (File::isCanceled) { //if true we stop the file wipe
                     outputFile.close(); //close the file for cancelation
-                    file.notify(true); //notify all observers that we finished the task
+                    file->notify(true); //notify all observers that we finished the task
                     return; //finish the function if we need to cancel
                 }
 
@@ -153,7 +84,7 @@ void File::WipeFile(const File& file, int passes, bool toRemove) {
                 if (outputFile.fail()) { //if true we failed to write data
                     outputFile.close(); //close the file due to error
                     File::setIsFailed(true); //set isFailed to true to indicate of failure
-                    file.notify(false); //notify all observers that we finished the task
+                    file->notify(false); //notify all observers that we finished the task
                     return; //finish the function if error occured
                 }
 
@@ -165,12 +96,13 @@ void File::WipeFile(const File& file, int passes, bool toRemove) {
         outputFile.close(); //after we finish we close the file
         if (toRemove) //if true we need to remove the file
             removeFile(file); //call removeFile function to remove the file
-        file.notify(true); //notify all observers that we finished the task
+        file->notify(true); //notify all observers that we finished the task
     }
     catch (const exception& e) { //catch exceptions that may be thrown
         outputFile.close(); //close the file due to error
         File::setIsFailed(true); //set isFailed to true to indicate of failure
-        file.notify(false); //notify all observers that we finished the task
+        file->notify(false); //notify all observers that we finished the task
+        cout << e.what() << endl; //print the error message
         return; //finish the function if error occured
     }
 }
@@ -178,25 +110,25 @@ void File::WipeFile(const File& file, int passes, bool toRemove) {
 
 /**
  * @brief Function that handles encryption/decryption on given file using custom AES library.
- * @param File file
+ * @param File* file
  * @param string key
  * @param bool decrypt
  */
-void File::CipherFile(const File& file, const string& key, bool decrypt) {
-    fstream outputFile(file.fullPath, ios::in | ios::out | ios::binary); //open the file in binary mode for reading and writing
+void File::CipherFile(File* file, const string& key, bool decrypt) {
+    fstream outputFile(filesystem::path(file->fullPath), ios::in | ios::out | ios::binary); //open the file in binary mode for reading and writing
 
     if (!outputFile.is_open() || outputFile.fail()) { //we check if we failed opening the file
         outputFile.close(); //close the file due to error
         File::setIsFailed(true); //set isFailed to true to indicate of failure
-        file.notify(false); //notify all observers that we finished the task
+        file->notify(false); //notify all observers that we finished the task
         return; //finish the function if error occured
     }
 
     try {
-        size_t fileSize = file.length; //set fileSize to be file size in bytes
+        size_t fileSize = file->length; //set fileSize to be file size in bytes
         size_t currentSize = 0; //set currentSize to be zero to indicate the beginning of file
         size_t chunkSize = 0; //set chunkSize to be zero and later calculate minimal chunk to read
-        const size_t maxBufferSize = 1024 * 1024; //set maxBufferSize to be 1MB for efficiency
+        const size_t maxBufferSize = 1024LL * 1024LL; //set maxBufferSize to be 1MB for efficiency
         size_t bufferSize = min(fileSize, maxBufferSize); //set minimal bufferSize for memory efficiency
         vector<char> buffer(bufferSize); //create buffer vector based on bufferSize
 
@@ -213,7 +145,7 @@ void File::CipherFile(const File& file, const string& key, bool decrypt) {
 
             if (File::isCanceled) { //if true we stop the file wipe
                 outputFile.close(); //close the file for cancelation
-                file.notify(true); //notify all observers that we finished the task
+                file->notify(true); //notify all observers that we finished the task
                 return; //finish the function if we need to cancel
             }
 
@@ -222,7 +154,7 @@ void File::CipherFile(const File& file, const string& key, bool decrypt) {
             if (outputFile.fail()) { //if true we failed to write data
                 outputFile.close(); //close the file due to error
                 File::setIsFailed(true); //set isFailed to true to indicate of failure
-                file.notify(false); //notify all observers that we finished the task
+                file->notify(false); //notify all observers that we finished the task
                 return; //finish the function if error occured
             }
 
@@ -237,7 +169,7 @@ void File::CipherFile(const File& file, const string& key, bool decrypt) {
             if (outputFile.fail()) { //if true we failed to write data
                 outputFile.close(); //close the file due to error
                 File::setIsFailed(true); //set isFailed to true to indicate of failure
-                file.notify(false); //notify all observers that we finished the task
+                file->notify(false); //notify all observers that we finished the task
                 return; //finish the function if error occured
             }
 
@@ -246,12 +178,13 @@ void File::CipherFile(const File& file, const string& key, bool decrypt) {
         }
 
         outputFile.close(); //after we finish we close the file
-        file.notify(true); //notify all observers that we finished the task
+        file->notify(true); //notify all observers that we finished the task
     }
     catch (const exception& e) { //catch exceptions that may be thrown
         outputFile.close(); //close the file due to error
         File::setIsFailed(true); //set isFailed to true to indicate of failure
-        file.notify(false); //notify all observers that we finished the task
+        file->notify(false); //notify all observers that we finished the task
+		cout << e.what() << endl; //print the error message
         return; //finish the function if error occured
     }
 }
